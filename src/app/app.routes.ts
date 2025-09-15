0b79ba6f6778f280461cb1fcb645cd95d45073ea
mario-rojas-reyes <223111157@upmh.edu.mx>
2025-09-15 15:37:37 -0600

    Mensaje del commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { PedirPrestamo } from './pedir-prestamo/pedir-prestamo';
+import { VistaCliente } from './vista-cliente/vista-cliente';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: Login },
+    { path: 'pedir-prestamo', component: PedirPrestamo },
+    { path: 'vista-cliente', component: VistaCliente },
+];
